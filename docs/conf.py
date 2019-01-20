23b817bbe0dcac58f9f58c6383955cdee86e5b5a
jdrumgoole <joe@joedrumgoole.com>
2019-01-20 00:49:34 +0000

    update

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'emacs'
+pygments_style = 'default'
 
 
 # -- Options for HTML output -------------------------------------------------
