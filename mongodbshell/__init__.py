512d1c4cb1cf0fd19acfff69279f264bbd409425
jdrumgoole <joe@joedrumgoole.com>
2019-04-14 12:14:06 +0100

    Fixed bug in docs

--- mongodbshell/__init__.py
+++ mongodbshell/__init__.py
@@ -10,31 +10,30 @@ the Python shell. Install using `pip3` (`MongoDBShell` only supports Python 3).
 
 ``$pip3 install mongodbshell``
 
-To use do:
+To use::
 
-``
->>> import mongodbshell
->>> client = mongodbshell.MongoDB()
->>> client.collection="test.test"
->>> client
-mongodbshell.MongoDB('test', 'test', 'mongodb://localhost:27017')
->>> client.insert_one({"msg" : "MongoDBShell is great"})
-ObjectId('5cb30cfa72a4ae3b105afa1c')
->>> client.find_one()
-1    {'_id': ObjectId('5cb30cfa72a4ae3b105afa1c'), 'msg': 'MongoDBShell is great'}
->>> client.line_numbers = 0
->>> client.find_one()
-{'_id': ObjectId('5cb30cfa72a4ae3b105afa1c'), 'msg': 'MongoDBShell is great'}
->>> # note the line number is no longer present
->>> client.output_file="output.txt" # send all output to this file
->>> client.find_one()
-Output is also going to 'output.txt'
-{'_id': ObjectId('5cb30cfa72a4ae3b105afa1c'), 'msg': 'MongoDBShell is great'}
->>> print(open("output.txt").read(), end="")
-{'_id': ObjectId('5cb30cfa72a4ae3b105afa1c'), 'msg': 'MongoDBShell is great'}
->>>
+    >>> import mongodbshell
+    >>> client = mongodbshell.MongoDB()
+    >>> client.collection="test.test"
+    >>> client
+    mongodbshell.MongoDB('test', 'test', 'mongodb://localhost:27017')
+    >>> client.insert_one({"msg" : "MongoDBShell is great"})
+    ObjectId('5cb30cfa72a4ae3b105afa1c')
+    >>> client.find_one()
+    1    {'_id': ObjectId('5cb30cfa72a4ae3b105afa1c'), 'msg': 'MongoDBShell is great'}
+    >>> client.line_numbers = 0
+    >>> client.find_one()
+    {'_id': ObjectId('5cb30cfa72a4ae3b105afa1c'), 'msg': 'MongoDBShell is great'}
+    >>> # note the line number is no longer present
+    >>> client.output_file="output.txt" # send all output to this file
+    >>> client.find_one()
+    Output is also going to 'output.txt'
+    {'_id': ObjectId('5cb30cfa72a4ae3b105afa1c'), 'msg': 'MongoDBShell is great'}
+    >>> print(open("output.txt").read(), end="")
+    {'_id': ObjectId('5cb30cfa72a4ae3b105afa1c'), 'msg': 'MongoDBShell is great'}
+    >>>
 
-``
+
 This will give you a prebuilt :py:class:`~MongoDBShell.MongoDB` object.
 
 """
