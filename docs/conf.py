eee873923e9cee7f37519fe77739d3061c732b8d
jdrumgoole <joe@joedrumgoole.com>
2019-05-02 21:43:09 +0100

    Version bump

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ project = 'MongoDBShell'
 author = 'Joe Drumgoole'
 
 # The short X.Y version
-version = '1.0.11'
+version = '1.0.12'
 # The full version, including alpha/beta/rc tags
-release = "1.0.11"
+release = "1.0.12"
 
 
 # -- General configuration ---------------------------------------------------
